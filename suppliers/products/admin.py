81b0e77f5be594a32c7109ae777ae2ab7d4d6826
Chanita Air Anuwong <chanita@odds.team>
2020-03-29 17:41:07 +0700

    register products app the new way

--- suppliers/products/admin.py
+++ suppliers/products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ('title', )
+
+
+admin.register(Product, ProductAdmin)
--- suppliers/suppliers/settings/base.py
+++ suppliers/suppliers/settings/base.py
@@ -29,16 +29,16 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
+    'products.apps.ProductsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
