e86306194b37cbf89cae4444a0a448c953e18538
Chanita Air Anuwong <chanita@odds.team>
2020-03-29 16:06:48 +0700

    database configuration in context-bounded settings

--- suppliers/suppliers/settings/base.py
+++ suppliers/suppliers/settings/base.py
@@ -70,20 +70,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'suppliers.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mypackage',
-        'USER': 'mypackageuser',
-        'PASSWORD': 'mypassword',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
